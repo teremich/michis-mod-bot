0a0b5f803c6fe825a56ae7035674b75d543cbd0c
michael tereschenko <michi.tereschenko@gmail.com>
2020-11-29 13:11:30 +0100

    further impovements, is working so far (except for the api request quota limit)

--- README.md
+++ README.md
@@ -10,12 +10,13 @@ What to install:
     -   pip install --upgrade google-auth-oauthlib google-auth-httplib2
 
 Make a Youtube account for the bot, give it mod rights.
+start with "python app.py"
 At start choose to log in via its account.
 
 TODO:
 
 -   bot
--   filter the chat for new messages
-    -   make the infinie loop (to not restart the bot every time we want to get chat's messages)
+-   make the infinie loop (to not restart the bot every time we want to get chat's messages)
 -   add commands and responses
 -   test our limits (use the bot in a real live stream)
+-   read the pinned messages in the developer's discord chat
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
+import time
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
 from commands import xp
+
+# This should be 'False', pls fix, if I uploaded it incorrectly
+TESTRUN = True
 
 # Commands
 commandsList = [
@@ -35,72 +39,100 @@ client_secrets_file = os.path.dirname(
     os.path.abspath(__file__))+"\\client_secret.json"
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
     client_secrets_file, scopes)
-credentials = flow.run_local_server()
+if not TESTRUN:
+    credentials = flow.run_console()
+else:
+    credentials = flow.run_local_server()
 youtube = googleapiclient.discovery.build(
     api_service_name, api_version, credentials=credentials)
 
-# Searching for Livestream by User with below written channelId
-request = youtube.search().list(
-    part="snippet",
-    channelId="UCvlsCHPqjj4Ydanpp_QZeOA",
-    eventType="live",
-    maxResults=1,
-    type="video"
-)
-response = request.execute()
-vidid = ""
-# Getting the VideoId or raising error if no livestream was found
-try:
-    vidid = response["items"][0]["id"]["videoId"]
-except IndexError:
-    raise Exception("NO LIVESTREAMS FOR THAT USER")
 
-# Getting id for the chat of the livestream
-request = youtube.videos().list(
-    part="snippet,contentDetails,statistics,liveStreamingDetails",
-    id=vidid
-)
-response = request.execute()
+def main():
+    while True:
+        # Searching for Livestream by User with below written channelId
+        request = youtube.search().list(
+            part="snippet",
+            channelId="UCvlsCHPqjj4Ydanpp_QZeOA",
+            eventType="live",
+            maxResults=1,
+            type="video"
+        )
+        response = request.execute()
+        vidid = ""
+        # Getting the VideoId or raising error if no livestream was found
+        try:
+            vidid = response["items"][0]["id"]["videoId"]
+        except IndexError:
+            print("No Livestream active")
+            time.sleep(60*5)
+        else:
+            # Getting id for the chat of the livestream
+            request = youtube.videos().list(
+                part="snippet,contentDetails,statistics,liveStreamingDetails",
+                id=vidid
+            )
+            response = request.execute()
+            if len(response["items"]) < 1:
+                continue
+            CHATID = response["items"][0]["liveStreamingDetails"]["activeLiveChatId"]
+            print(CHATID)
+            newestChatId = ""
+            while True:
+                try:
+                    # Getting 2000 messages from youtube
+                    request = youtube.liveChatMessages().list(
+                        liveChatId=CHATID,
+                        part="id,snippet,authorDetails",
+                        maxResults=2000
+                    )
+                    response = request.execute()
+                    if "error" in response.keys():
+                        raise IndexError(
+                            "Could not read Chat Messages, trying to reconnect...")
 
-CHATID = response["items"][0]["liveStreamingDetails"]["activeLiveChatId"]
-print(CHATID)
-# Getting 2000 messages from youtube
-request = youtube.liveChatMessages().list(
-    liveChatId=CHATID,
-    part="id,snippet,authorDetails",
-    maxResults=2000
-)
-response = request.execute()
+                    def sendText(text, tag="NULL"):
+                        if tag == "NULL":
+                            msgText = text[:200]
+                        else:
+                            msgText = "@{0} -> {1}".format(tag, text)[:200]
+                        request = youtube.liveChatMessages().insert(
+                            part="snippet",
+                            body={
+                                "snippet": {
+                                    "liveChatId": CHATID,
+                                    "type": "textMessageEvent",
+                                    "textMessageDetails": {
+                                            "messageText": msgText
+                                    }
+                                }
+                            }
+                        )
+                        response = request.execute()
+                        if TESTRUN:
+                            print(response)
+                        if "error" in response.keys():
+                            raise IndexError(
+                                "Could not write a message to Chat, trying to reconnect...")
+
+                    i = 0
+                    for i in range(len(response["items"])-1, -1, -1):
+                        if response["items"][i]["id"] == newestChatId:
+                            break
+                    for j in range(i, len(response["items"])):
+                        message = response["items"][j]
+                        # listenForSpam()
+                        # listenForWords(message)
+                        for activator in activatorWords:
+                            if (activator["word"] in message["snippet"]["textMessageDetails"]["messageText"]):
+                                sendText(activator["response"],
+                                         message["authorDetails"]["displayName"])
+                        # listenForCommands(message)
+                        for com in commandsList:
+                            com({"sendText": sendText}, message)
+                    newestChatId = response["items"][-1]["id"]
+                except IndexError:
+                    break
 
 
-def sendText(text, tag="NULL"):
-    if tag == "NULL":
-        msgText = text[:200]
-    else:
-        msgText = "@{0} -> {1}".format(tag, text)[:200]
-    request = youtube.liveChatMessages().insert(
-        part="snippet",
-        body={
-            "snippet": {
-                "liveChatId": CHATID,
-                "type": "textMessageEvent",
-                "textMessageDetails": {
-                        "messageText": msgText
-                }
-            }
-        }
-    )
-    response = request.execute()
-    # print(response)
-
-
-# printing the message texts.
-msgs = response["items"]
-#msgs = msgs[::-1]
-for message in msgs:
-    for activator in activatorWords:
-        if (activator["word"] in message["snippet"]["textMessageDetails"]["messageText"]):
-            sendText(activator["response"],
-                     message["authorDetails"]["displayName"])
-    for com in commandsList:
-        com({"sendText": sendText}, message)
+if __name__ == "__main__":
+    main()
